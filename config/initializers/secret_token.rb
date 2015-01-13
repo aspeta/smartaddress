9b6ce0a33a430abbbf4244b312b6961ae956ae18
aspeta <aspeta@yahoo.com>
2015-01-13 22:08:16 +0000

    adding safe address

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Smartaddress::Application.config.secret_key_base = '0936f39d1519897f9994d56b0be283b73cf1ad2845ca5a41761e5080ebc5c9e855d00851cd20a2bb4e2563559680c90477bbd46ec83fe414f862f315c81fe61c'
